72e31a783dbabd97287f9d2c42621103d34d720a
fahdfady <fahd.fady212@gmail.com>
2025-02-16 11:59:47 +0200

    feat: fn set source code

--- src/lib.rs
+++ src/lib.rs
@@ -35,7 +35,17 @@ impl Compiler {
         }
     }
 
-    pub async fn Compile(&mut self) -> Result<JsValue, JsValue> {
+    pub fn set_source_code(&mut self, source_code: &str) -> Result<(), JsValue> {
+        let mut state = self.compilation_state.borrow_mut();
+
+        state.source_code = source_code.to_string();
+        state.last_error = None;
+        state.output = None;
+
+        Ok(())
+    }
+
+    pub async fn compile(&mut self) -> Result<JsValue, JsValue> {
         let state = self.compilation_state.borrow();
 
         let res = format!("Compilation done for source code: {}", state.source_code);
